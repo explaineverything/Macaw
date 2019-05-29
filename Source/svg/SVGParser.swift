55ec8433855a6a26363a608134a9afa933317ce8
Lukasz Krystek <lukasz.krystek@explaineverything.com>
2019-05-29 16:48:16 +0200

    Fix for unwrapping nil value - contentNode in parsing pattern.

--- Source/svg/SVGParser.swift
+++ Source/svg/SVGParser.swift
@@ -394,7 +394,7 @@ open class SVGParser {
             contentNode = Group(contents: shapes)
         }
 
-        return UserSpacePattern(content: contentNode!, bounds: bounds, userSpace: userSpace, contentUserSpace: contentUserSpace)
+        return UserSpacePattern(content: contentNode ?? Group(), bounds: bounds, userSpace: userSpace, contentUserSpace: contentUserSpace)
     }
 
     fileprivate func parseGroup(_ group: XMLIndexer, style: [String: String]) throws -> Group? {
